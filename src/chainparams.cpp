45c5efd7e2d2c9d474b28ccbd39e2acb2583dda1
Simon Sedlatschek <s.sedlatschek@telekom.de>
2021-05-15 18:54:19 +0200

    add oli as peer

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -121,6 +121,7 @@ public:
         // service bits we want, but we should get them updated to support all service bits wanted by any
         // release ASAP to avoid it where possible.
         vSeeds.emplace_back("sued.download");
+        vSeeds.emplace_back("91.159.122.115");
 
         base58Prefixes[PUBKEY_ADDRESS] = std::vector<unsigned char>(1,55);
         base58Prefixes[SCRIPT_ADDRESS] = std::vector<unsigned char>(1,5);
