25ea3d2dbd49175624585900ab6981e94d92c01a
Simon Sedlatschek <s.sedlatschek@telekom.de>
2021-05-15 16:09:17 +0200

    update dns and nodes

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -120,10 +120,7 @@ public:
         // This is fine at runtime as we'll fall back to using them as a oneshot if they don't support the
         // service bits we want, but we should get them updated to support all service bits wanted by any
         // release ASAP to avoid it where possible.
-        vSeeds.emplace_back("seed-a.pfandcoin.loshan.co.uk");
-        vSeeds.emplace_back("dnsseed.thrasher.io");
-        vSeeds.emplace_back("dnsseed.pfandcointools.com");
-        vSeeds.emplace_back("dnsseed.pfandcoinpool.org");
+        vSeeds.emplace_back("sued.download");
 
         base58Prefixes[PUBKEY_ADDRESS] = std::vector<unsigned char>(1,55);
         base58Prefixes[SCRIPT_ADDRESS] = std::vector<unsigned char>(1,5);
